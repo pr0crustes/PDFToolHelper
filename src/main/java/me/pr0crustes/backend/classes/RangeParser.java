4b89c8d7f3e48a8a8c64cddf34bf6e1dd5217cc0
pr0crustes <dark.pr0crustes@gmail.com>
2018-08-12 16:45:01 -0300

    Changing arrays inside RangeParser to Sets. RangeParser has current no use.

--- src/main/java/me/pr0crustes/backend/classes/RangeParser.java
+++ src/main/java/me/pr0crustes/backend/classes/RangeParser.java
@@ -1,9 +1,6 @@
 package me.pr0crustes.backend.classes;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-
-import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,13 +13,10 @@ public class RangeParser {
         this.rangeString = rangeString;
     }
 
-    public Integer[] getValues() {
+    public Set<Integer> getValues() {
         Set<Integer> parsedValues = new HashSet<>();
 
-        String[] rangesParts = RangeParser.getMatches(rangeString, "\\d+_\\d+");
-        String[] addParts = RangeParser.getMatches(rangeString, "[+]\\d+");
-        String[] subParts = RangeParser.getMatches(rangeString, "[-]\\d+");
-
+        Set<String> rangesParts = RangeParser.getMatches(rangeString, "\\d+_\\d+");
         for (String str : rangesParts) {
             String[] parts = str.split("_");
             for (int i = Integer.valueOf(parts[0]); i <= Integer.valueOf(parts[1]); i++) {
@@ -30,30 +24,32 @@ public class RangeParser {
             }
         }
 
+        Set<String> addParts = RangeParser.getMatches(rangeString, "[+]\\d+");
         for (String str : addParts) {
             parsedValues.add(RangeParser.stringAsUnsigned(str));
         }
 
+        Set<String> subParts = RangeParser.getMatches(rangeString, "[-]\\d+");
         for (String str : subParts) {
             parsedValues.remove(RangeParser.stringAsUnsigned(str));
         }
 
-        return parsedValues.toArray(new Integer[0]);
+        return parsedValues;
     }
 
     private static int stringAsUnsigned(String str) {
         return Integer.valueOf(str.replace("-", "").replace("+", ""));
     }
 
-    private static String[] getMatches(String str, String regex) {
-        List<String> matches = new ArrayList<>();
+    private static Set<String> getMatches(String str, String regex) {
+        Set<String> matches = new HashSet<>();
         Matcher match = Pattern.compile(regex).matcher(str);
 
         while (match.find()) {
             matches.add(match.group());
         }
 
-        return matches.toArray(new String[0]);
+        return matches;
     }
 
 }
